20a0bd6f0957fe8fe26caa2d20a9ab667e38608c
Aldana Torres <aldana.torres@irrigacion.gov.ar>
2021-05-19 15:59:23 -0300

    se modifico EditarActor.tsx

--- src/actores/EditarActor.tsx
+++ src/actores/EditarActor.tsx
@@ -6,29 +6,30 @@ import { actorCreacionDTO, actorDTO } from "./actor.model";
 import FormularioActor from "./FormularioActor";
 
 export default function EditarActor() {
-    const transformar = (actor: actorDTO) => {
-        return {
-            nombre: actor.nombre,
-            fotoURL: actor.foto,
-            biografia: actor.biografia,
-            fechaNacimiento: new Date(actor.fechaNacimiento)
-        }
-    }
-    return (
-        <>
-            <h3>Editar Actor</h3>
-
-           <EditarEntidad<actorCreacionDTO, actorDTO>
-                url={urlActores} urlIndice="/actores" nombreEntidad="Actores"
-                transformarFormData={convertirActorAFormData}
-                transformar={transformar}
-                >
-                {(entidad, editar) =>
-                    <FormularioActor
-                        modelo={entidad}
-                        onSubmit={async valores => await editar(valores)}
-                    />}
-            </EditarEntidad>
-        </>
-    )
+  const transformar = (actor: actorDTO) => {
+    return {
+      nombre: actor.nombre,
+      fotoURL: actor.foto,
+      biografia: actor.biografia,
+      fechaNacimiento: new Date(actor.fechaNacimiento),
+    };
+  };
+  return (
+    <>
+      <EditarEntidad<actorCreacionDTO, actorDTO>
+        url={urlActores}
+        urlIndice="/actores"
+        nombreEntidad="Actores"
+        transformarFormData={convertirActorAFormData}
+        transformar={transformar}
+      >
+        {(entidad, editar) => (
+          <FormularioActor
+            modelo={entidad}
+            onSubmit={async (valores) => await editar(valores)}
+          />
+        )}
+      </EditarEntidad>
+    </>
+  );
 }
